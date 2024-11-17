8d2386e3dd8a64be4196e19e43328deff5d946ca
asombre <34612529+asombre@users.noreply.github.com>
2024-11-17 07:49:55 -0700

    Created dvdinstall.sh

---
+++ dvdinstall.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+os=rhel
+major=9
+minor=5
+vmname=$os$major$minor\iso-$(date +%s)
+cpunum=4
+memnum=4096
+imgpath=/var/lib/libvirt/images
+isopath=$imgpath/$os-$major.$minor-x86_64-dvd.iso
+diskpath=$imgpath/$vmname.qcow2,size=10
+ksfile=dvdinstall.ks
+kspath=ksdir/$ksfile
+
+virt-install \
+	--name $vmname \
+	--vcpus $cpunum \
+	--memory $memnum \
+	--location $isopath \
+	--disk $diskpath \
+	--initrd-inject $kspath \
+  --extra-args inst.ks="file:/$ksfile console=tty0 console=ttyS0,115200" \
+	--osinfo $os$major.$minor \
+  --nographics 
