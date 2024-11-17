2f044849696fe9286ab2fae9a37deb42e0c68c47
asombre <34612529+asombre@users.noreply.github.com>
2024-11-17 07:53:08 -0700

    Created netinstall.sh

---
+++ netinstall.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+os=rhel
+major=9
+minor=5
+vmname=$os$major$minor\net-$(date +%s)
+cpunum=4
+memnum=4096
+repoip=x.x.x.x
+imgpath=/var/lib/libvirt/images
+pkgpath=http://$repoip/$os/$major.$minor/
+diskpath=$imgpath/$vmname.qcow2,size=10
+ksfile=netinstall.ks
+
+virt-install \
+	--name $vmname \
+	--vcpus $cpunum \
+	--memory $memnum \
+	--location $pkgpath \
+	--disk $diskpath \
+  --extra-args inst.ks="http://$repoip/kickstarts/$ksfile console=tty0 console=ttyS0,115200" \
+	--osinfo $os$major.$minor \
+  --nographics 
