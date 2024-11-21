3d1e3f725c9b0d2ccdfe82e9e124b68fa6310cfb
asombre <34612529+asombre@users.noreply.github.com>
2024-11-20 20:39:11 -0700

    Updated imageinstall.sh
    Added more virt-customize options.

--- imageinstall.sh
+++ imageinstall.sh
@@ -12,7 +12,16 @@ echo "exporting..."
 export LIBGUESTFS_BACKEND=direct
 
 echo "creating kvm image..."
-virt-customize -a $kvmdir/$vmname.qcow2 --root-password password:password1234 --uninstall cloud-init
+virt-customize -a $kvmdir/$vmname.qcow2 \
+--root-password password:password1234 \
+--hostname $vmname \
+--copy-in scripts/userpass.sh:/root \
+--copy-in scripts/sshkeygen.sh:/root \
+--copy-in scripts/my.repo:/etc/yum.repos.d/ \
+--firstboot-command 'useradd user -s /bin/bash -c "First Last" \
+--firstboot-command 'yum clean all; yum makecache' \
+--firstboot-command 'vim-enhanced' \
+--uninstall cloud-init
 
 echo "changing owner..."
 chown qemu:qemu $kvmdir/$vmname.qcow2
