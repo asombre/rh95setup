071384a923807502822419d506bcd122e217af58
asombre <34612529+asombre@users.noreply.github.com>
2024-11-20 20:39:49 -0700

    Updated imageinstall.sh
    Added quote.

--- imageinstall.sh
+++ imageinstall.sh
@@ -18,7 +18,7 @@ virt-customize -a $kvmdir/$vmname.qcow2 \
 --copy-in scripts/userpass.sh:/root \
 --copy-in scripts/sshkeygen.sh:/root \
 --copy-in scripts/my.repo:/etc/yum.repos.d/ \
---firstboot-command 'useradd user -s /bin/bash -c "First Last" \
+--firstboot-command 'useradd user -s /bin/bash -c "First Last"' \
 --firstboot-command 'yum clean all; yum makecache' \
 --firstboot-command 'vim-enhanced' \
 --uninstall cloud-init
